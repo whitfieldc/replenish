55993ecd58425608b6f94a59bc5507af8dc628bc
Hugh Sutton-Gee <hsuttong@gmail.com>
2015-07-10 13:35:49 -0700

    -Remove unused image assets -Remove stupid font text files -Clean up js files, mostly adding semicolons missing from many lines.   -Fixed a bad invocation of preventDefault, which was missing ()'s.   -Remove dead commented out code, debugger statments etc. -Clean up CSS/SCSS removing commented out lines

--- app/assets/javascripts/firebaseAuth.js
+++ app/assets/javascripts/firebaseAuth.js
@@ -1,19 +1,17 @@
-$(document).ready(function(){
-  if (localStorage['firebase:session::sdg']){
-    console.log('logging in with previous data')
+$(document).ready(function() {
+  if (localStorage['firebase:session::sdg']) {
+    console.log('logging in with previous data');
     var oldAuthData = JSON.parse(localStorage['firebase:session::sdg']);
     ajaxLogin(oldAuthData);
   }
   $('#login-button').on('click', function(e) {
-
     e.preventDefault();
     console.log("prevent this shit");
-
-    googleAuth().then(ajaxLogin)
+    googleAuth().then(ajaxLogin);
   });
-})
+});
 
-var ajaxLogin = function(aData){
+var ajaxLogin = function(aData) {
   $auth_id = aData.uid;
   $user_name = aData.google.displayName;
   bindForm();
@@ -21,71 +19,65 @@ var ajaxLogin = function(aData){
   bindDestruction();
 
   $.ajax({
-    type: "POST",
+    type: 'POST',
     url: '/create',
     data: { "auth_id": $auth_id, "name": $user_name },
-  }).done(function(resp){
-    $("#card").css("display", "block")
-    $("#card").append(resp)
-    $("#index-salmon").empty()
-    $("#index-bowl").empty()
-    $("#index-onion").empty()
-    $(".cycle-circles").empty()
-    // $("#login-button").empty()
-    // $("#login-button").css("background-color", "transparent")
-    // this removes the login button but we should actually replace with logout
-    $("#login-button").css('display', 'none')
-    $("header").css("background-color", "#43A047")
-    $("footer").css("background-color", "#43A047")
-    // $("body").css("background-image", "url()")
-    $("body").css("background-color", "#ECEFF1")
-  })
-}
+  }).done(function(resp) {
+    $("#card").css("display", "block");
+    $("#card").append(resp);
+    $("#index-salmon").empty();
+    $("#index-bowl").empty();
+    $("#index-onion").empty();
+    $(".cycle-circles").empty();
 
-var bindForm = function(){
-  $(document).on('click', ':submit#addButton', function(e){
+    // this removes the login button but we should actually replace with logout
+    $("#login-button").css('display', 'none');
+    $("header").css("background-color", "#43A047");
+    $("footer").css("background-color", "#43A047");
+    $("body").css("background-color", "#ECEFF1");
+  });
+};
+
+var bindForm = function() {
+  $(document).on('click', ':submit#addButton', function(e) {
     e.preventDefault();
     addItem(e);
-  })
-}
+  });
+};
 
-var bindUpdates = function(){
+var bindUpdates = function() {
   $(document).on('click', '.checkbox-icon', function(e){
     e.preventDefault();
-    var checkbox = $(e.target)
-    var card = $(e.target).closest('.item-card')
-    var id = $(e.target).closest('.item-card').attr('id')
+    var checkbox = $(e.target);
+    var card = $(e.target).closest('.item-card');
+    var id = $(e.target).closest('.item-card').attr('id');
     var update = $.ajax({
-      url: "/itemsusers/" + id,
-      type: "PUT"
-    })
-    update.done(function(response){
-      // console.log(response)
-      checkbox.attr('class', "fa fa-check-square-o fa-lg checkbox-icon")
-      checkbox.attr('style', 'color:#43A047')
-      // debugger
-    })
-  })
-}
+      url: '/itemsusers/' + id,
+      type: 'PUT'
+    });
+    update.done(function(response) {
+      checkbox.attr('class', "fa fa-check-square-o fa-lg checkbox-icon");
+      checkbox.attr('style', 'color:#43A047');
+    });
+  });
+};
 
-var bindDestruction = function(){
-  $(document).on('click', '.delete-icon', function(e){
-    e.preventDefault
-    var card = $(e.target).closest('.item-card')
-    var id = $(e.target).closest('.item-card').attr('id')
+var bindDestruction = function() {
+  $(document).on('click', '.delete-icon', function(e) {
+    e.preventDefault();
+    var card = $(e.target).closest('.item-card');
+    var id = $(e.target).closest('.item-card').attr('id');
     var destroy = $.ajax({
-      url: "/itemsusers/" + id,
-      type: "DELETE"
-    })
-    destroy.done(function(response){
+      url: '/itemsusers/' + id,
+      type: 'DELETE'
+    });
+    destroy.done(function(response) {
       card.remove();
-    })
-  })
-}
+    });
+  });
+};
 
-// bindForm($('some-submit-css'));
-
-function addItem(event){
+function addItem(event) {
   var form = $('#addForm');
   var data = form.serialize();
   // console.log(data);
@@ -93,47 +85,36 @@ function addItem(event){
   // var description =
   // var name =
   $.ajax({
-    url: "/items",
-    type: "POST",
-    data: data//{"name": $name, "description": $description, "category": $category}
-  })
-  .done(function(response){
-    // console.log(response);
-    $("#name-field").val("")
-    $("#description-field").val("")
-    $("#category-field").val("")
-    $('.item-cards-list').append(response)
-    var newCard = $('.item-cards-list').children().last()
-    var data = { id: newCard.attr('id') }
+    url: '/items',
+    type: 'POST',
+    data: data //{"name": $name, "description": $description, "category": $category}
+  }).done(function(response) {
+    $("#name-field").val("");
+    $("#description-field").val("");
+    $("#category-field").val("");
+    $('.item-cards-list').append(response);
+    var newCard = $('.item-cards-list').children().last();
+    var data = { id: newCard.attr('id') };
     $.ajax({
-      url: "/itemsusers",
-      type: "POST",
+      url: '/itemsusers',
+      type: 'POST',
       data: data
-    }).done(function(response){
-      // console.log(response)
-    })
-  })
+    }).done(function(response) {
+    });
+  });
 }
 
-function googleAuth(){
-  var promise = new Promise(function(resolve, reject){
+function googleAuth() {
+  var promise = new Promise(function(resolve, reject) {
     var ref = new Firebase("https://sdg.firebaseio.com");
     ref.authWithOAuthPopup("google", function(error, authData) {
       if (error) {
         console.log("Login Failed!", error);
-        reject(error)
+        reject(error);
       } else {
-        resolve(authData)
-      };
-    })
-
-  })
+        resolve(authData);
+      }
+    });
+  });
   return promise;
 }
-
-
-
-
-
-
-
